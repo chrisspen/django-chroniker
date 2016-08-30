cdbdc090b29ebc32bba4f4fe902b20c362c73c1b
chrisspen <chrisspen@gmail.com>
2016-08-30 17:56:46 -0400

    Fixed exception thrown when deletion disabled site-wide, reported in issue #60.

--- chroniker/admin.py
+++ chroniker/admin.py
@@ -682,7 +682,8 @@ class MonitorAdmin(admin.ModelAdmin):
     
     def get_actions(self, request):
         actions = super(MonitorAdmin, self).get_actions(request)
-        del actions['delete_selected']
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
         return actions
     
     def has_delete_permission(self, request, obj=None):
