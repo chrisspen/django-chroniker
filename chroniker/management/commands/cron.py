6c39aeb0bd0e9203adb86cf358a37c8f86f0e043
darkwizz <psyhoge@gmail.com>
2016-09-26 16:31:10 +0300

    removed CommandParser import

--- chroniker/management/commands/cron.py
+++ chroniker/management/commands/cron.py
@@ -10,7 +10,7 @@ from optparse import make_option
 from collections import defaultdict
 import subprocess
 
-from django.core.management.base import BaseCommand, CommandParser
+from django.core.management.base import BaseCommand
 from django.db import connection
 import django
 from django.conf import settings
