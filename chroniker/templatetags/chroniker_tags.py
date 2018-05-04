07b3e9fc44d3842edb753c545f29ebe87c4162e4
mbande <mohsenbande@gmail.com>
2018-05-04 23:17:25 +0430

    Update chroniker_tags.py

--- chroniker/templatetags/chroniker_tags.py
+++ chroniker/templatetags/chroniker_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from django.core.urlresolvers import reverse, NoReverseMatch
+from django.urls import reverse, NoReverseMatch
 from django.utils import timezone
 from django.conf import settings
 
