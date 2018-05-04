3c8a4d97413c9c3f8f02219c082c4587896fd070
mbande <mohsenbande@gmail.com>
2018-05-04 23:32:38 +0430

    Update widgets.py

--- chroniker/widgets.py
+++ chroniker/widgets.py
@@ -93,26 +93,26 @@ class ForeignKeyTextInput(TextInput):
 
 class VerboseForeignKeyRawIdWidget(ForeignKeyRawIdWidget):
     def label_for_value(self, value):
-        key = self.rel.get_related_field().name
+        key = self.remote_field.get_related_field().name
         try:
-            obj = self.rel.to._default_manager.using(self.db).get(**{key: value})
+            obj = self.remote_field.to._default_manager.using(self.db).get(**{key: value})
             change_url = reverse(
                 "admin:%s_%s_change" % (obj._meta.app_label, obj._meta.object_name.lower()),
                 args=(obj.pk,)
             )
             return '&nbsp;<strong><a href="%s" target="_blank">%s</a></strong>' \
                 % (change_url, escape(obj))
-        except (ValueError, self.rel.to.DoesNotExist):
+        except (ValueError, self.remote_field.to.DoesNotExist):
             return ''
 
 class VerboseManyToManyRawIdWidget(ManyToManyRawIdWidget):
     def label_for_value(self, value):
         values = value.split(',')
         str_values = []
-        key = self.rel.get_related_field().name
+        key = self.remote_field.get_related_field().name
         for v in values:
             try:
-                obj = self.rel.to._default_manager.using(self.db).get(**{key: v})
+                obj = self.remote_field.to._default_manager.using(self.db).get(**{key: v})
                 x = smart_text(obj)
                 change_url = reverse(
                     "admin:%s_%s_change" % (obj._meta.app_label, obj._meta.object_name.lower()),
@@ -120,7 +120,7 @@ class VerboseManyToManyRawIdWidget(ManyToManyRawIdWidget):
                 )
                 str_values += ['<strong><a href="%s" target="_blank">%s</a></strong>' \
                     % (change_url, escape(x))]
-            except self.rel.to.DoesNotExist:
+            except self.remote_field.to.DoesNotExist:
                 str_values += ['???']
         return ', '.join(str_values)
 
@@ -128,7 +128,7 @@ class ImproveRawIdFieldsForm(admin.ModelAdmin):
     def formfield_for_dbfield(self, db_field, **kwargs):
         if db_field.name in self.raw_id_fields:
             kwargs.pop("request", None)
-            typ = db_field.rel.__class__.__name__
+            typ = db_field.remote_field.__class__.__name__
             if typ == "ManyToOneRel":
                 kwargs['widget'] = VerboseForeignKeyRawIdWidget(db_field.rel, site)
             elif typ == "ManyToManyRel":
@@ -140,7 +140,7 @@ class ImproveRawIdFieldsFormTabularInline(admin.TabularInline):
     def formfield_for_dbfield(self, db_field, **kwargs):
         if db_field.name in self.raw_id_fields:
             kwargs.pop("request", None)
-            typ = db_field.rel.__class__.__name__
+            typ = db_field.remote_field.__class__.__name__
             if typ == "ManyToOneRel":
                 kwargs['widget'] = VerboseForeignKeyRawIdWidget(db_field.rel, site)
             elif typ == "ManyToManyRel":
