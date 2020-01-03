2e640f9071e4ad5c70da275a6b4aff90337fc8a2
Xarv <strzelecki20@gmail.com>
2020-01-03 15:49:25 +0100

    fix six packaged and render_to_response->render

--- chroniker/admin.py
+++ chroniker/admin.py
@@ -6,7 +6,7 @@ from django.core.management import get_commands
 from django.urls import reverse, NoReverseMatch
 from django.db import models
 from django.forms import TextInput
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.utils.encoding import force_text
 from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.template.defaultfilters import linebreaks
@@ -385,7 +385,7 @@ class JobAdmin(admin.ModelAdmin):
             'max_duration': max_duration,
         }
 
-        return render_to_response('admin/chroniker/job/duration_graph.html', context, request)
+        return render(request, 'admin/chroniker/job/duration_graph.html', context)
 
     def get_urls(self):
         urls = super(JobAdmin, self).get_urls()
