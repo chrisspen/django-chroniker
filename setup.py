cbb2a8a2c85655334efb6bf752247a848d78496b
F. Malina <fmalina@pm.me>
2021-02-10 20:45:32 +0100

    Revert

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def get_reqs(*fns):
 
 
 setup(
-    name="chroniker",
+    name="django-chroniker",
     version=chroniker.__version__,
     packages=find_packages(),
     scripts=['bin/chroniker'],
