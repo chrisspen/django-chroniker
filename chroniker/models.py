90c069e6c3adfc3d52ce14dd38ac7e0201586588
Rolf Fokkens <rolf.fokkens@thunderbyte.ai>
2024-03-28 14:15:05 +0100

    Raising an exception on a non zero return status kills all stderr. Let's try this.

--- chroniker/models.py
+++ chroniker/models.py
@@ -1047,7 +1047,7 @@ class Job(models.Model):
                 assert next_run != _next_run, 'RRule failed to increment next run datetime.'
             # next_run = next_run.replace(tzinfo=timezone.get_current_timezone())
 
-            last_run_successful = not _raw_status && not bool(stderr.length)
+            last_run_successful = not _raw_status and not bool(stderr.length)
 
             try:
                 with lock:
