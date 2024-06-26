50b1f15ba1aa3c9685fed626ccdc53c95b7023cf
chrisspen <chrisspen@gmail.com>
2024-06-26 15:18:11 -0400

    Updated min Django version.

--- chroniker/__init__.py
+++ chroniker/__init__.py
@@ -1,8 +1,2 @@
-from django import VERSION as DJANGO_VERSION
-
-
-VERSION = (1, 0, 23)
+VERSION = (1, 0, 24)
 __version__ = '.'.join(map(str, VERSION))
-
-if DJANGO_VERSION < (3, 2):
-    default_app_config = 'chroniker.apps.ChronikerConfig'
